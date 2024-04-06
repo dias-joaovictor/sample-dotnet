1614a4dcf764dc58685153d077c586594f6e26ad
João Victor Dias <joao.dias@bestseller.com>
2024-04-06 22:13:20 +0200

    add gitignore

--- obj/Debug/net8.0/SampleDotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleDotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleDotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d458aa823247319d4992a5d2b47ef394dce916e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleDotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleDotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
